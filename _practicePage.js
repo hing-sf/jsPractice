fcca82a380fc00e700320a0a82d34bc089c690c8
Hing Tang <hingtang@hings-mbp.lan>
2020-12-08 13:43:01 -0800

    fn random year, material, diameter

--- _practicePage.js
+++ _practicePage.js
@@ -1,39 +1,338 @@
-// Given a sorted array, remove the duplicates in-place such that each element appear only once and return the new length.
-// Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
-
-// Example
-// Given nums = [1,1,2],
-// Your function should return length = 2, with the first two elements of nums being 1 and 2 respectively.
-// It doesn't matter what you leave beyond the new length.
-
-var nums = [1, 1, 2];
-// var myArray = ['a', 1, 'a', 2, '1'];
-const myArray = ['ab', 'string', 'e33h', 'd1', 'ab', 'e2', 'e33', 'd2'];
-
-// Option 1
-var removeDuplicates = function (nums) {
-	var sortedArray = nums.sort();
-	var len = sortedArray.length - 1;
-	var newArr = [];
-
-	if (len >= 0) {
-		for (var i = 0; i < len; i++) {
-			if (sortedArray[i] !== sortedArray[i + 1]) {
-				newArr.push(sortedArray[i]);
+const json = {
+	type: 'FeatureCollection',
+	features: [
+		{
+			type: 'Feature',
+			geometry: {
+				type: 'MultiLineString',
+				coordinates: [
+					[
+						[-122.271408, 37.558588],
+						[-122.270613, 37.558133],
+						[-122.26992, 37.557773],
+						[-122.269487, 37.557559],
+						[-122.269061, 37.557346],
+						[-122.268895, 37.557258],
+						[-122.268802, 37.557207]
+					]
+				]
+			},
+			properties: {
+				year: 2020,
+				diameter: 4,
+				native_pipe_id: '1',
+				material_type_code: 2
+			}
+		},
+		{
+			type: 'Feature',
+			geometry: {
+				type: 'MultiLineString',
+				coordinates: [
+					[
+						[-122.268801, 37.557206],
+						[-122.268647, 37.557133],
+						[-122.26846, 37.557037],
+						[-122.268396, 37.557005],
+						[-122.26819, 37.556906],
+						[-122.268084, 37.556849],
+						[-122.267864, 37.556737],
+						[-122.267787, 37.556698],
+						[-122.267483, 37.556538],
+						[-122.267405, 37.556493],
+						[-122.267293, 37.556428],
+						[-122.267293, 37.556428]
+					]
+				]
+			},
+			properties: {
+				year: 2020,
+				diameter: 4,
+				native_pipe_id: '2',
+				material_type_code: 2
+			}
+		},
+		{
+			type: 'Feature',
+			geometry: {
+				type: 'MultiLineString',
+				coordinates: [
+					[
+						[-122.267295, 37.55643],
+						[-122.267067, 37.55662],
+						[-122.26694, 37.556775],
+						[-122.266809, 37.556915],
+						[-122.266642, 37.557099],
+						[-122.266493, 37.557248],
+						[-122.266394, 37.557392],
+						[-122.266235, 37.557613],
+						[-122.265951, 37.557932],
+						[-122.265538, 37.558399]
+					]
+				]
+			},
+			properties: {
+				year: 2017,
+				diameter: 4,
+				native_pipe_id: '3',
+				material_type_code: 2
+			}
+		},
+		{
+			type: 'Feature',
+			geometry: {
+				type: 'MultiLineString',
+				coordinates: [
+					[
+						[-122.265533, 37.5584],
+						[-122.265328, 37.558628],
+						[-122.265119, 37.558887],
+						[-122.26496, 37.559032],
+						[-122.26496, 37.559032]
+					]
+				]
+			},
+			properties: {
+				year: 2018,
+				diameter: 4,
+				native_pipe_id: '4',
+				material_type_code: 2
+			}
+		},
+		{
+			type: 'Feature',
+			geometry: {
+				type: 'MultiLineString',
+				coordinates: [
+					[
+						[-122.264961, 37.559033],
+						[-122.265245, 37.559161],
+						[-122.265563, 37.559351],
+						[-122.265991, 37.559595]
+					]
+				]
+			},
+			properties: {
+				year: 2018,
+				diameter: 4,
+				native_pipe_id: '5',
+				material_type_code: 2
+			}
+		},
+		{
+			type: 'Feature',
+			geometry: {
+				type: 'MultiLineString',
+				coordinates: [
+					[
+						[-122.265986, 37.559588],
+						[-122.266986, 37.560156],
+						[-122.267973, 37.560691],
+						[-122.268205, 37.56081],
+						[-122.269096, 37.561347]
+					]
+				]
+			},
+			properties: {
+				year: 2018,
+				diameter: 4,
+				native_pipe_id: '6',
+				material_type_code: 2
+			}
+		},
+		{
+			type: 'Feature',
+			geometry: {
+				type: 'MultiLineString',
+				coordinates: [
+					[
+						[-122.2691, 37.561345],
+						[-122.269348, 37.56106],
+						[-122.269723, 37.560595],
+						[-122.270013, 37.560169],
+						[-122.270386, 37.559763],
+						[-122.270666, 37.559359],
+						[-122.271169, 37.558821],
+						[-122.271407, 37.558582]
+					]
+				]
+			},
+			properties: {
+				year: 2019,
+				diameter: 4,
+				native_pipe_id: '7',
+				material_type_code: 2
+			}
+		},
+		{
+			type: 'Feature',
+			geometry: {
+				type: 'MultiLineString',
+				coordinates: [
+					[
+						[-122.271401, 37.558589],
+						[-122.271573, 37.558372],
+						[-122.271869, 37.558072],
+						[-122.272047, 37.55791],
+						[-122.272273, 37.557706],
+						[-122.272585, 37.557486],
+						[-122.273024, 37.557216],
+						[-122.273225, 37.557045],
+						[-122.273674, 37.556787],
+						[-122.274463, 37.556306]
+					]
+				]
+			},
+			properties: {
+				year: 2015,
+				diameter: 4,
+				native_pipe_id: '8',
+				material_type_code: 2
+			}
+		},
+		{
+			type: 'Feature',
+			geometry: {
+				type: 'MultiLineString',
+				coordinates: [
+					[
+						[-122.27446, 37.556303],
+						[-122.27517, 37.555869],
+						[-122.275848, 37.555442],
+						[-122.276484, 37.554996],
+						[-122.277213, 37.554524],
+						[-122.277481, 37.55436]
+					]
+				]
+			},
+			properties: {
+				year: 2015,
+				diameter: 4,
+				native_pipe_id: '9',
+				material_type_code: 2
+			}
+		},
+		{
+			type: 'Feature',
+			geometry: {
+				type: 'MultiLineString',
+				coordinates: [
+					[
+						[-122.277478, 37.55435],
+						[-122.277474, 37.554362],
+						[-122.278124, 37.553961],
+						[-122.278611, 37.5536],
+						[-122.278945, 37.553373],
+						[-122.279664, 37.552775],
+						[-122.280151, 37.552281],
+						[-122.280371, 37.552031]
+					]
+				]
+			},
+			properties: {
+				year: 2015,
+				diameter: 4,
+				native_pipe_id: '10',
+				material_type_code: 2
+			}
+		},
+		{
+			type: 'Feature',
+			geometry: {
+				type: 'MultiLineString',
+				coordinates: [
+					[
+						[-122.280377, 37.552023],
+						[-122.28103, 37.55141],
+						[-122.281498, 37.55093],
+						[-122.28188, 37.550534],
+						[-122.282577, 37.549845],
+						[-122.282965, 37.549421],
+						[-122.283083, 37.549313],
+						[-122.283284, 37.549007],
+						[-122.283398, 37.548742],
+						[-122.283462, 37.548578],
+						[-122.283669, 37.548056],
+						[-122.28392, 37.547392],
+						[-122.28413, 37.546913],
+						[-122.284293, 37.546713],
+						[-122.284544, 37.546501],
+						[-122.284792, 37.546363],
+						[-122.285041, 37.546176]
+					]
+				]
+			},
+			properties: {
+				year: 2015,
+				diameter: 4,
+				native_pipe_id: '11',
+				material_type_code: 2
+			}
+		},
+		{
+			type: 'Feature',
+			geometry: {
+				type: 'MultiLineString',
+				coordinates: [
+					[
+						[-122.280355, 37.552058],
+						[-122.280024, 37.551836],
+						[-122.279483, 37.551599],
+						[-122.279069, 37.551493],
+						[-122.278598, 37.551483],
+						[-122.278057, 37.551579],
+						[-122.277681, 37.55172],
+						[-122.27735, 37.551907],
+						[-122.276873, 37.552139],
+						[-122.276344, 37.552447],
+						[-122.275981, 37.552654]
+					]
+				]
+			},
+			properties: {
+				year: 2015,
+				diameter: 4,
+				native_pipe_id: '12',
+				material_type_code: 2
 			}
 		}
-		newArr.push(sortedArray[len]);
-	}
-	return newArr;
+	]
 };
 
-console.log(removeDuplicates(myArray));
+let parsedYears = {};
+let geoJsonList = [];
+for (const el of json.features) {
+	let year = el.properties.year;
+	if (parsedYears[year]) {
+		parsedYears[year] = [...parsedYears[year], el];
+	} else {
+		parsedYears[year] = [el];
+	}
+}
 
-// Option 2
-var option2 = myArray.filter((value, idx, arr) => arr.indexOf(value) === idx);
+for (const [k, v] of Object.entries(parsedYears)) {
+	const layer = `${k}Layer`;
 
-console.log(option2); // unique is ['a', 1, 2, '1']
+	let geoJson = [
+		{
+			[k]: {
+				type: 'geojson',
+				data: {
+					id: k,
+					type: 'FeatureCollection',
+					features: v
+				}
+			},
+			[layer]: {
+				id: k,
+				type: 'line',
+				paint: {
+					'line-color': 'red'
+				}
+			}
+		}
+	];
+	geoJsonList = [...geoJsonList, geoJson];
+}
 
-// Option 3
-let option3 = [...new Set(myArray)];
-console.log(option3); // unique is ['a', 1, 2, '1']
+console.log('geoJsonList', geoJsonList);
